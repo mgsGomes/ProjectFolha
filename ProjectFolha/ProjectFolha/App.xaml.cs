987a9ec1963fa36290528dfbd4591b2a9d64a3bd
Micael Gomes de Souza <micael@metadados.com.br>
2017-03-22 02:15:10 -0300

    Montagem inicial do MasterDetailPage e estruturação de pastas do modelo MVVM.

--- ProjectFolha/ProjectFolha/App.xaml.cs
+++ ProjectFolha/ProjectFolha/App.xaml.cs
@@ -2,13 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace ProjectFolha
 {
     public partial class App : Application
     {
+        public static MasterDetailPage MasterDetail { get; set; }
+
+        public async static Task NavigateMasterDetail(Page page)
+        {
+            App.MasterDetail.IsPresented = false;
+            await App.MasterDetail.Detail.Navigation.PushAsync(page);
+        }
+
         public App()
         {
             InitializeComponent();
--- ProjectFolha/ProjectFolha/Detail.xaml.cs
+++ ProjectFolha/ProjectFolha/Detail.xaml.cs
@@ -3,18 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace ProjectFolha
 {
-    public partial class MainPage : MasterDetailPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Detail : ContentPage
     {
-        public MainPage()
+        public Detail()
         {
             InitializeComponent();
-            this.Master = new Master();
-            this.Detail = new NavigationPage(new Detail());
-            App.MasterDetail = this;
         }
     }
 }
