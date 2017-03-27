4875e55300ca7afde88a3583be18a626f3009521
Micael Gomes de Souza <micael@metadados.com.br>
2017-03-27 01:23:37 -0300

    Definição do layout geral e implementação da interface INotifyPropertyChange

---
+++ ProjectFolha/ProjectFolha/Model/Entities/ObservableBaseObject.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectFolha.Model.Entities
+{
+    public class ObservableBaseObject : INotifyPropertyChanged
+    {
+        public ObservableBaseObject()
+        {
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate
+        {
+
+        };
+
+        public void OnPropertyChange([CallerMemberName] string Name = "")
+        {
+            if (PropertyChanged == null)
+                return;
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(Name));
+        }
+    }
+}
